4bccff401fc0756614dd26830e6bcdf42471f237
stetaf <s.tafuri95@gmail.com>
2021-08-17 16:07:11 +0200

    Add menu icons

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,13 +3,31 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-4">
-                <h2 class="logo">Boolgram</h2>
+                <a href="#">
+                    <h2 class="logo">Boolgram</h2>
+                </a>
+            </div>
+            <div class="col-4 text-center">
+                <input class="search" name="search" id="search" type="text" placeholder="Cerca">
             </div>
             <div class="col-4">
-                <input class="search" name="search" id="search" type="text" placeholder="&#xF002; Cerca">
-            </div>
-            <div class="col-4">
-                <h2>Usermenu</h2>
+                <div class="menu d-flex justify-content-end">
+                    <a href="#">
+                        <i class="fas fa-home"></i>
+                    </a>
+                    <a href="#">
+                        <i class="far fa-paper-plane"></i>
+                    </a>
+                    <a href="#">
+                        <i class="far fa-compass"></i>
+                    </a>
+                    <a href="#">
+                        <i class="far fa-heart"></i>
+                    </a>
+                    <a href="#">
+                        <i class="far fa-user-circle"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
