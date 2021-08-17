2beff9f6b9cebb41523afa6dffd7ad13fe5e2608
stetaf <s.tafuri95@gmail.com>
2021-08-17 15:28:42 +0200

    Create header structure and style

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,17 @@
+<!-- Header -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h2 class="logo">Boolgram</h2>
+            </div>
+            <div class="col-4">
+                <h2>Search</h2>
+            </div>
+            <div class="col-4">
+                <h2>Usermenu</h2>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- /Header -->
