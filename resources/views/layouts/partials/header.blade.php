62e86b1499e0ef36fa409d16e80d667bdeb854be
stetaf <s.tafuri95@gmail.com>
2021-08-17 15:55:59 +0200

    Fix searchbar placeholder font-family

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -6,7 +6,7 @@
                 <h2 class="logo">Boolgram</h2>
             </div>
             <div class="col-4">
-                <input class="search" name="search" id="search" type="text" placeholder="&#xF002; Cerca" style="font-family: Arial, 'Font Awesome 5 Free'" />
+                <input class="search" name="search" id="search" type="text" placeholder="&#xF002; Cerca">
             </div>
             <div class="col-4">
                 <h2>Usermenu</h2>
