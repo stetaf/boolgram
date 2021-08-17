1aaa6b0eb6cefd8ac26b16b0f6c4ca39b1aaeb13
stetaf <s.tafuri95@gmail.com>
2021-08-17 15:54:30 +0200

    Add header searchbar

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,12 +1,12 @@
 <!-- Header -->
 <header>
     <div class="container">
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-4">
                 <h2 class="logo">Boolgram</h2>
             </div>
             <div class="col-4">
-                <h2>Search</h2>
+                <input class="search" name="search" id="search" type="text" placeholder="&#xF002; Cerca" style="font-family: Arial, 'Font Awesome 5 Free'" />
             </div>
             <div class="col-4">
                 <h2>Usermenu</h2>
