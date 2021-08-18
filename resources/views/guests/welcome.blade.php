642ee64db59cbf132f5db639ee0b2c76e52338ee
stetaf <s.tafuri95@gmail.com>
2021-08-18 14:37:00 +0200

    Create base guests home

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <h1>Boolgram</h1>
-
+<div class="container">
+    <div class="welcome">
+        <div class="left">
+            <div class="phones">
+                <img src="https://www.instagram.com/static/images/homepage/screenshot5.jpg/0a2d3016f375.jpg" alt="">
+            </div>
+        </div>
+        <div class="right">
+            <div class="card text-center">
+                <h2 class="logo">Boolgram</h2>
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="username" placeholder="Enter your username">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="password" placeholder="Enter your password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Accedi</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
