28fd90a87adb0ad741c468d4cec4290839ab2479
stetaf <s.tafuri95@gmail.com>
2021-08-18 15:16:11 +0200

    Edit login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,70 +2,62 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+    <div class="welcome">
+        <div class="left">
+            <div class="phones">
+                <img src="https://www.instagram.com/static/images/homepage/screenshot5.jpg/0a2d3016f375.jpg" alt="">
+            </div>
+        </div>
+        <div class="right">
+            <div class="card text-center">
+                <h2 class="logo mb-4">Boolgram</h2>
+                <form method="POST" action="{{ Route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" id="email" placeholder="Enter your email" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Enter your password" required autocomplete="current-password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn form-control">Log In</button>
+                </form>
+                <div class="d-flex justify-content-center align-items-center mt-2">
+                    <div class="separator"></div>
+                    <span class="o">o</span>
+                    <div class="separator"></div>
                 </div>
+                <a href="#" class="mt-2 d-flex justify-content-center align-items-center font-weight-bold">
+                    <i class="fab fa-facebook-square"></i>
+                    Sign in with Facebook
+                </a>
+                @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}" class="fs-12 mt-3">
+                        Forgot password ?
+                    </a>
+                @endif
+            </div>
+            <div class="card justify-content-center flex-row mt-2">
+                <span>Don't have an account ?</span>
+                <a href="#" class="ml-2 text-primary font-weight-bold">Signup</a>
+            </div>
+            <span class="d-block w-100 text-center mt-3">Get the app.</span>
+            <div class="d-flex justify-content-center mt-3">
+                <a href="#">
+                    <img src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_italian-it.png/9078ec632936.png" alt="iOS appstore" height="40">
+                </a>
+                <a href="#">
+                    <img src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_italian-it.png/a27e4240699a.png" class="ml-2" alt="Google appstore" height="40">
+                </a>
             </div>
         </div>
     </div>
