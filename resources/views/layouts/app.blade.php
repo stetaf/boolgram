9325355688df8f6f76a50ea88451155ab3d5e4c8
stetaf <s.tafuri95@gmail.com>
2021-08-18 14:36:04 +0200

    Render header only on auth check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,14 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css' integrity='sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==' crossorigin='anonymous' />
 </head>
 <body>
-    @include('layouts.partials.header')
+    @if(Auth::check())
+        @include('layouts.partials.header')
+    @endif
 
-    @yield('content')
+    <!-- Main -->
+    <main>
+        @yield('content')
+    </main>
+    <!-- /Main -->
 </body>
 </html>
